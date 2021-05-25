e16cbad5887512a93f5a81a417bc309b0fa63ea4
GiancarloGonzalez2001 <49995630+GiancarloGonzalez2001@users.noreply.github.com>
2021-05-25 16:11:12 -0500

    Delete consultas.py

--- Proyecto_BD/consultas.py
+++
@@ -1,9 +0,0 @@
-#Cantidad de jugadores por universidad
-def prueba():
-    return """select uni.nombre as universidad, count(jug.nombre) as cantidad
-        from universidad uni join jugador jug on (jug.id_universidad = uni.id)
-        group by uni.nombre
-        order by cantidad desc"""
-
-# def consulta():
-#     return """consulta escrita en sql"""
